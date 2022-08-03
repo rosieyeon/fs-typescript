9abcf97f309daffe5cc1084f3cb04b0990e01412
rosieyeon <sseyeon956@gmail.com>
2022-08-04 03:05:39 +0900

    feat: add summoner page layout

--- src/pages/Summoners.styled.ts
+++ src/pages/Summoners.styled.ts
@@ -6,3 +6,24 @@ export const SummonersLogo = styled.img`
   width: 500px;
   margin: 0 auto;
 `;
+
+export const SummonersList = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
+  padding: 40px 0;
+`;
+export const SummonersBox = styled.div``;
+export const SummonersImg = styled.img`
+  width: 300px;
+  height: auto;
+  cursor: pointer;
+`;
+export const SummonersName = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  padding: 12px;
+  text-align: center;
+  cursor: pointer;
+`;
--- src/pages/Summoners.tsx
+++ src/pages/Summoners.tsx
@@ -2,7 +2,15 @@ import React from "react";
 
 import { useNavigate } from "react-router-dom";
 import { t1Player } from "app/t1Player";
-import { SummonersLayout, SummonersLogo } from "./Summoners.styled";
+import {
+  SummonersBox,
+  SummonersImg,
+  SummonersLayout,
+  SummonersList,
+  SummonersLogo,
+  SummonersName,
+} from "./Summoners.styled";
+import toCapitalize from "util/toCapitalize";
 
 const Summoners: React.FC = () => {
   const navigate = useNavigate();
@@ -15,11 +23,22 @@ const Summoners: React.FC = () => {
   return (
     <SummonersLayout>
       <SummonersLogo src="images/logo/t1.jpeg" />
-      {t1PlayerList.map((t1Player, idx) => (
-        <button key={idx} onClick={() => onClickNav(`${t1Player.player}`)}>
-          {t1Player.player}
-        </button>
-      ))}
+      <SummonersList>
+        {t1PlayerList.map((t1Player, idx) => (
+          <SummonersBox key={idx}>
+            <SummonersImg
+              src={`/images/players/${t1Player.player}.jpg`}
+              onClick={() => onClickNav(`${t1Player.player}`)}
+            />
+            <SummonersName
+              key={idx}
+              onClick={() => onClickNav(`${t1Player.player}`)}
+            >
+              {toCapitalize(t1Player.player)}
+            </SummonersName>
+          </SummonersBox>
+        ))}
+      </SummonersList>
     </SummonersLayout>
   );
 };

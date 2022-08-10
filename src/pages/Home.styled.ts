9e3573d44221c01b1a249322ef985fcaacb2e806
rosieyeon <sseyeon956@gmail.com>
2022-08-10 17:38:54 +0900

    style: delete annotations

--- src/pages/Home.styled.ts
+++ src/pages/Home.styled.ts
@@ -4,77 +4,8 @@ export const HomeLayout = styled.div`
   font-family: 'Roboto', sans-serif;
 `;
 
-export const HomeNavbar = styled.div``;
-
 export const HomeLogo = styled.img`
   display: block;
   width: 500px;
-  margin: 0 auto;
-`;
-
-export const HomeSearch = styled.div`
-  text-align: center;
-`;
-export const HomeSearchBar = styled.input`
-  width: 40%;
-  height: 35px;
-  border-radius: 6px;
-  padding: 10px;
-  border: none;
-`;
-export const HomeSearchButton = styled.button`
-  height: 35px;
-  border-radius: 6px;
-  margin-left: 10px;
-  width: 100px;
-  background-color: #d12d35;
-  color: white;
-  cursor: pointer;
-  border: none;
-`;
-export const HomeYoutubeItem = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 15px;
-  margin: 0 auto;
-
-  width: 90%;
-  padding: 60px 0;
-`;
-
-export const HomeLoading = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 15px;
-  margin: 0 auto;
-
-  width: 90%;
-  padding: 60px 0;
-`;
-export const HomeLoadingSkeleton = styled.div`
-  width: 100%;
-`;
-export const HomeLoadingImg = styled.div`
-  ${({ theme }) => theme.skeletonGradation}
-
-  border-radius: 4px;
-  margin-bottom: 8px;
-  aspect-ratio: 16/9;
-`;
-export const HomeLoadingTxt = styled.div`
-  ${({ theme }) => theme.skeletonGradation}
-  width: 100%;
-  height: 20px;
-  border-radius: 4px;
-
-  padding: 10px 0 20px 0;
-`;
-
-export const HomeError = styled.div`
-  width: 90%;
-  height: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   margin: 0 auto;
 `;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,83 +1,11 @@
 import React from 'react';
 
-// import { useAppDispatch, useAppSelector } from 'app/store';
-// import YoutubeItem from 'components/youtube/YoutubeItem';
-// import { getYoutubeList } from 'features/youtube/youtubeListSlice';
-import {
-  // HomeError,
-  HomeLayout,
-  // HomeLoading,
-  // HomeLoadingImg,
-  // HomeLoadingSkeleton,
-  // HomeLoadingTxt,
-  HomeLogo,
-  // HomeSearch,
-  // HomeSearchBar,
-  // HomeSearchButton,
-  // HomeYoutubeItem,
-} from './Home.styled';
+import { HomeLayout, HomeLogo } from './Home.styled';
 
 const Home: React.FC = () => {
-  // const [keyword, setKeyword] = useState('');
-  // const [query, setQuery] = useState('');
-  // const { youtubeList, loading, error } = useAppSelector(
-  //   (state) => state.youtubeList
-  // );
-
-  // const dispatch = useAppDispatch();
-
-  // 여기에 then으로 에러 분기 처리
-  // useEffect(() => {
-  //   dispatch(getYoutubeList(query));
-  // }, [dispatch, query]);
-
-  //TODO useCallback 이용이유 확실하게 써야하는 이유, 쓸 때에는 dependency 걸어야하는지 기준점
-  // const onChangeSearch = useCallback(
-  //   (event: { target: { value: React.SetStateAction<string> } }) => {
-  //     setKeyword(event.target.value);
-  //   },
-  //   []
-  // );
-
-  // const onClickApply = useCallback(() => {
-  //   setQuery(keyword);
-  // }, [keyword]);
-
-  // const onKeyPress = (event: { key: string }) => {
-  //   if (event.key === 'Enter') {
-  //     onClickApply();
-  //   }
-  // };
-
   return (
     <HomeLayout>
       <HomeLogo src="images/logo/t1.jpeg" />
-      {/* <HomeSearch>
-        <HomeSearchBar
-          placeholder="검색"
-          onChange={onChangeSearch}
-          onKeyPress={onKeyPress}
-        />
-        <HomeSearchButton onClick={onClickApply}>SEARCH</HomeSearchButton>
-      </HomeSearch>
-      {loading === 'pending' ? (
-        <HomeLoading>
-          {youtubeList.map((youtube, index) => (
-            <HomeLoadingSkeleton key={index}>
-              <HomeLoadingImg />
-              <HomeLoadingTxt />
-            </HomeLoadingSkeleton>
-          ))}
-        </HomeLoading>
-      ) : error ? (
-        <HomeError>데이터를 불러올 수 없습니다</HomeError>
-      ) : (
-        <HomeYoutubeItem>
-          {youtubeList.map((youtube, index) => (
-            <YoutubeItem youtube={youtube} key={index} />
-          ))}
-        </HomeYoutubeItem>
-      )} */}
     </HomeLayout>
   );
 };

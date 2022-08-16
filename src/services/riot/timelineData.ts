c382422c82078127359491a8bb5252d9228ee12c
rosieyeon <sseyeon956@gmail.com>
2022-08-16 18:05:45 +0900

    wip: draw a gold graph

--- src/services/riot/timelineData.ts
+++ src/services/riot/timelineData.ts
@@ -1,16 +1,14 @@
-import { Tracing } from 'trace_events';
+// interface DetailDataDto {
+//   frameInterval: number;
+//   frames: FramesDto[];
+//   gameId: number;
+//   participants: ParticipantsDto[];
+// }
 
-interface DetailDataDto {
-  frameInterval: number;
-  frames: FramesDto[];
-  gameId: number;
-  participants: ParticipantsDto[];
-}
-
-interface ParticipantsDto {
-  participantId: number;
-  puuid: string;
-}
+// interface ParticipantsDto {
+//   participantId: number;
+//   puuid: string;
+// }
 
 interface FramesDto {
   events: []; //TODO 여기에 뭐라고 써야하지? itemevent? levelevent?
@@ -30,42 +28,42 @@ interface FramesDto {
 }
 
 // TODO 일단 type 기준으로 나누는 것으로 생각하기
-interface ItemEventsDto {
-  itemId: number;
-  participantId: number;
-  timestamp: number;
-  type: string;
-}
+// interface ItemEventsDto {
+//   itemId: number;
+//   participantId: number;
+//   timestamp: number;
+//   type: string;
+// }
 
-interface LevelEventsDto {
-  levelUpType: string;
-  participantId: number;
-  skillSlot: number;
-  timestamp: number;
-  type: string;
-}
+// interface LevelEventsDto {
+//   levelUpType: string;
+//   participantId: number;
+//   skillSlot: number;
+//   timestamp: number;
+//   type: string;
+// }
 
-interface WardKillEventsDto {
-  killerId: number;
-  timestamp: number;
-  type: string;
-  wardType: string;
-}
+// interface WardKillEventsDto {
+//   killerId: number;
+//   timestamp: number;
+//   type: string;
+//   wardType: string;
+// }
 
-interface WardCreateEventsDto {
-  creatorId: number;
-  timestamp: number;
-  type: string;
-  wardType: string;
-}
+// interface WardCreateEventsDto {
+//   creatorId: number;
+//   timestamp: number;
+//   type: string;
+//   wardType: string;
+// }
 
-interface KillEventsDto {
-  killType: string;
-  killerId: number;
-  position: { x: number; y: number };
-  timestamp: number;
-  type: string;
-}
+// interface KillEventsDto {
+//   killType: string;
+//   killerId: number;
+//   position: { x: number; y: number };
+//   timestamp: number;
+//   type: string;
+// }
 
 interface ParticipantsFramesDto {
   championStats: [];
@@ -100,16 +98,16 @@ export const goldFrameData = (data: FramesDto[]) => {
   return data.map((item: FramesDto, index) => {
     return {
       time: index,
-      1: item.participantFrames[1].totalGold,
-      2: item.participantFrames[2].totalGold,
-      3: item.participantFrames[3].totalGold,
-      4: item.participantFrames[4].totalGold,
-      5: item.participantFrames[5].totalGold,
-      6: item.participantFrames[6].totalGold,
-      7: item.participantFrames[7].totalGold,
-      8: item.participantFrames[8].totalGold,
-      9: item.participantFrames[9].totalGold,
-      10: item.participantFrames[10].totalGold,
+      1: item.participantFrames[1].totalGold / 1000,
+      2: item.participantFrames[2].totalGold / 1000,
+      3: item.participantFrames[3].totalGold / 1000,
+      4: item.participantFrames[4].totalGold / 1000,
+      5: item.participantFrames[5].totalGold / 1000,
+      6: item.participantFrames[6].totalGold / 1000,
+      7: item.participantFrames[7].totalGold / 1000,
+      8: item.participantFrames[8].totalGold / 1000,
+      9: item.participantFrames[9].totalGold / 1000,
+      10: item.participantFrames[10].totalGold / 1000,
     };
   });
 };
